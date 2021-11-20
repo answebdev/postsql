1fcd85363664ced0b0a937e9f0ed65c402898ec1
Adolf <britvaica@netscape.net>
2021-11-19 19:28:56 -0800

    add validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
+const { validateRegisterInput } = require('../../util/validators');
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
@@ -11,7 +12,16 @@ module.exports = {
       parent,
       { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // Validate user data
+      // Validate user data ('validateRegisterInput' is from 'validators.js' file in 'util' folder)
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
       // Make sure user does not already exist
       const user = await User.findOne({ username });
       if (user) {
---
+++ util/validators.js
@@ -0,0 +1,32 @@
+module.exports.validateRegisterInput = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (username.trim() === '') {
+    errors.username = 'Username field must not be empty';
+  }
+  if (email.trim() === '') {
+    errors.email = 'Email field must not be empty';
+  } else {
+    // Check if email is valid
+    const regEx =
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = 'Email must be a valid email address';
+    }
+  }
+  if (password === '') {
+    errors.password = 'Password field must not empty';
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = 'Passwords must match';
+  }
+
+  return {
+    errors,
+    // If smaller than 1, that means there are no errors, i.e., the data is valid:
+    valid: Object.keys(errors).length < 1,
+  };
+};
